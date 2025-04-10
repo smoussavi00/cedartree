15a9f3d84f173fea397853c65698367f5d58f9e8
smoussavi00 <smoussavi.c@gmail.com>
2025-04-09 20:56:59 -0400

    server app + package updates

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ app.get('/main', (req,res) => {
     else if (!req.session.verified) return res.redirect('/verify');
     
     res.sendFile(path.join(__dirname,'public','main.html'));
+
 });
 
 app.get('/settings', (req,res) => {
@@ -84,34 +85,6 @@ app.get('/change', (req,res) => {
     res.sendFile(path.join(__dirname,'public','change.html'));
 });
 
-app.get('/api/session', (req, res) => {
-    res.json({email: req.session.email || null });
-});
-
-app.get('/api/code', async (req, res) => {
-
-    if (!req.session.email) return res.status(401).send({ message: 'notloggedin' });
-
-    const user = await User.findOne({ email: req.session.email });
-    res.send({ code: user.code, codeexp: user.codeexp });
-    
-});
-
-app.post('/api/resend', async (req, res) => {
-
-    const {email} = req.body;
-    
-    const newcode = codegen();
-    const newcodeexp = new Date(Date.now() + 15 * 60 * 1000);
-
-    await User.findOneAndUpdate(
-        { email: email },
-        { code: newcode, codeexp: newcodeexp }, 
-        { new: true, runValidators: true } 
-    );
-
-});
-
 app.post('/login', async (req, res) => {
 
     const { email, password } = req.body;
@@ -124,6 +97,7 @@ app.post('/login', async (req, res) => {
     if (!match) return res.status(401).send({message: 'badpass'});
     
     if (!user.verified) return res.status(401).send({message: 'unverified'});
+    
 
     req.session.email = user.email;
     req.session.verified = user.verified;
@@ -135,8 +109,16 @@ app.post('/create', async (req, res) => {
 
     const { email, password } = req.body;
     
-    const exists = await User.findOne({ email: email.toLowerCase(), verified: true });
+    var exists = await User.findOne({ email: email.toLowerCase(), verified: true });
     if(exists) return res.status(400).send({ message: 'existsemail' });
+    // if user exists but not verified, delete it
+
+    exists = await User.findOne({ email: email.toLowerCase(), verified: false });
+    if(exists){
+        await User.findOneAndDelete({ email: email.toLowerCase(), verified: false });
+        req.session.email = null;
+        req.session.verified = null;
+    }
 
     const saltRounds = 10; 
     const salt = await bcrypt.genSalt(saltRounds);
@@ -157,6 +139,64 @@ app.post('/create', async (req, res) => {
     req.session.verified = newuser.verified;
 
     await newuser.save();
+    
+    //send email with code
+    //sendmail(email, code);
+
+});
+
+app.get('/api/session', (req, res) => {
+    res.json({
+        email: req.session.email || null,
+        verified: req.session.verified || null
+    });
+});
+
+app.get('/api/code', async (req, res) => {
+
+    if (!req.session.email) return res.status(401).send({ message: 'notloggedin' });
+    if (req.session.verified) return res.status(401).send({ message: 'alreadyverified' });
+
+    const user = await User.findOne({ email: req.session.email });
+    res.send({ code: user.code, codeexp: user.codeexp });
+    
+});
+
+app.post('/api/resend', async (req, res) => {
+
+    if (!req.session.email) return res.status(401).send({ message: 'notloggedin' });
+    if (req.session.verified) return res.status(401).send({ message: 'alreadyverified' });
+
+    const { email } = req.body;
+    
+    const newcode = codegen();
+    const newcodeexp = new Date(Date.now() + 15 * 60 * 1000);
+
+    await User.findOneAndUpdate(
+        { email: email },
+        { code: newcode, codeexp: newcodeexp }, 
+        { new: true, runValidators: true } 
+    );
+
+    //send email with code
+    //sendmail(email, newcode);
+
+});
+
+app.post('/api/verify', async (req, res) => {   
+
+    if (!req.session.email) return res.status(401).send({ message: 'notloggedin' });
+    if (req.session.verified) return res.status(401).send({ message: 'alreadyverified' });
+
+    const { email } = req.body;
+
+    await User.findOneAndUpdate(
+        { email: email },
+        { verified: true }, 
+        { new: true, runValidators: true } 
+    );
+    req.session.verified = true;
+    res.send({ message: 'goodverify' });
 
 });
 
@@ -167,53 +207,3 @@ app.listen(port, () => {
 function codegen() {
     return Math.floor(100000 + Math.random() * 900000).toString(); 
 }
-
-
-
-/*
-
-async function testhash(){
-
-    password = "johnny14";
-
-    const saltRounds = 10; 
-    const salt = await bcrypt.genSalt(saltRounds);
-    const passhash = await bcrypt.hash(password, salt);
-
-    console.log("password: " + password);
-    console.log("passhash: " + passhash);
-
-}
-
-async function testdate(){
-    var x = new Date(Date.now() + 15 * 60 * 1000);
-}
-
-testdate();
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://amusawi:tiat650TI@cedarcluster.f2ukivh.mongodb.net/?retryWrites=true&w=majority&appName=cedarcluster";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-*/
