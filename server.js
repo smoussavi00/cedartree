1719c00cbdbcdddf92e1c99e95a86be6e018f9d2
smoussavi00 <smoussavi.c@gmail.com>
2025-04-10 21:35:03 -0400

    server app changes

--- server.js
+++ server.js
@@ -40,10 +40,12 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/login', (req,res) =>{
+    if(req.session.loggedin) return res.redirect('/main');
     res.sendFile(path.join(__dirname,'public','login.html'));
 });
 
 app.get('/create', (req,res) => {
+    if(req.session.loggedin) return res.redirect('/main');
     res.sendFile(path.join(__dirname,'public','create.html'));
 });
 
@@ -53,6 +55,7 @@ app.get('/forgot', (req,res) => {
 
 app.get('/verify', (req,res) => {
 
+    if(req.session.loggedin) return res.redirect('/main');
     if(!req.session.email) return res.redirect('/login');
     if(req.session.verified) return res.redirect('/main');
     
@@ -61,27 +64,29 @@ app.get('/verify', (req,res) => {
 });
 
 app.get('/reset', (req,res) => {
+    if(loggedin) return res.redirect('/main');
+    if(!req.session.email || !req.session.resetallowed) return res.redirect('/login');
+
     res.sendFile(path.join(__dirname,'public','reset.html'));
 });
 
 app.get('/main', (req,res) => {
-    
-    if (!req.session.email) return res.redirect('/login');
-    else if (!req.session.verified) return res.redirect('/verify');
-    
+    if (!req.session.loggedin) return res.redirect('/login');
     res.sendFile(path.join(__dirname,'public','main.html'));
-
 });
 
 app.get('/settings', (req,res) => {
+    if (!req.session.loggedin) return res.redirect('/login');
     res.sendFile(path.join(__dirname,'public','settings.html'));
 });
 
 app.get('/delete', (req,res) => {
+    if (!req.session.loggedin) return res.redirect('/login');
     res.sendFile(path.join(__dirname,'public','delete.html'));
 });
 
 app.get('/change', (req,res) => {
+    if (!req.session.loggedin) return res.redirect('/login');
     res.sendFile(path.join(__dirname,'public','change.html'));
 });
 
@@ -94,13 +99,15 @@ app.post('/login', async (req, res) => {
     if (!user) return res.status(401).send({message: 'bademail'});
     
     const match = await bcrypt.compare(password, user.passhash);
-    if (!match) return res.status(401).send({message: 'badpass'});
-    
-    if (!user.verified) return res.status(401).send({message: 'unverified'});
-    
+    if (!match) return res.status(401).send({message: 'badpassw'});
 
     req.session.email = user.email;
     req.session.verified = user.verified;
+    
+    if (!user.verified) return res.status(401).send({message: 'unverified'});
+
+    req.session.loggedin = true;
+    
     res.send({ message: 'goodlogin' });
 
 });
@@ -110,10 +117,12 @@ app.post('/create', async (req, res) => {
     const { email, password } = req.body;
     
     var exists = await User.findOne({ email: email.toLowerCase(), verified: true });
-    if(exists) return res.status(400).send({ message: 'existsemail' });
-    // if user exists but not verified, delete it
+
+    if(req.session.email) req.session.email = null;
+    if(req.session.verified) req.session.verified = null;
 
     exists = await User.findOne({ email: email.toLowerCase(), verified: false });
+
     if(exists){
         await User.findOneAndDelete({ email: email.toLowerCase(), verified: false });
         req.session.email = null;
@@ -140,10 +149,42 @@ app.post('/create', async (req, res) => {
 
     await newuser.save();
     
+    res.send({ message: 'goodcreate' });
     //send email with code
     //sendmail(email, code);
 
 });
+
+app.post('/forgot', async (req, res) => {  
+    
+    const { email } = req.body;
+    
+    const user = await User.findOne({ email: email.toLowerCase() });
+    if(!user) return res.status(401).send({ message: 'bademail' });
+
+    if(!user.verified){
+        req.session.email = user.email;
+        req.session.verified = user.verified;
+        return res.status(401).send({ message: 'unverified' });
+    }
+
+    if(req.session.email) req.session.email = null;
+    if(req.session.verified) req.session.verified = null;
+
+    const newcode = codegen();
+    const newcodeexp = new Date(Date.now() + 15 * 60 * 1000);
+    await User.findOneAndUpdate(
+        { email: email.toLowerCase() },
+        { code: newcode, codeexp: newcodeexp }, 
+        { new: true, runValidators: true } 
+    );
+
+    //send email with code
+    //sendmail(email, newcode);
+
+    res.send({ message: 'goodforgot' });
+}
+);
 
 app.get('/api/session', (req, res) => {
     res.json({
@@ -152,22 +193,14 @@ app.get('/api/session', (req, res) => {
     });
 });
 
-app.get('/api/code', async (req, res) => {
-
-    if (!req.session.email) return res.status(401).send({ message: 'notloggedin' });
-    if (req.session.verified) return res.status(401).send({ message: 'alreadyverified' });
-
-    const user = await User.findOne({ email: req.session.email });
-    res.send({ code: user.code, codeexp: user.codeexp });
-    
-});
-
 app.post('/api/resend', async (req, res) => {
+    
+    const { email, forgot } = req.body;
 
-    if (!req.session.email) return res.status(401).send({ message: 'notloggedin' });
-    if (req.session.verified) return res.status(401).send({ message: 'alreadyverified' });
-
-    const { email } = req.body;
+    if (!forgot){
+        if (!req.session.email) return res.status(401).send({ message: 'noemail' });
+        if (req.session.verified) return res.status(401).send({ message: 'yesverified' });
+    }
     
     const newcode = codegen();
     const newcodeexp = new Date(Date.now() + 15 * 60 * 1000);
@@ -185,19 +218,65 @@ app.post('/api/resend', async (req, res) => {
 
 app.post('/api/verify', async (req, res) => {   
 
-    if (!req.session.email) return res.status(401).send({ message: 'notloggedin' });
-    if (req.session.verified) return res.status(401).send({ message: 'alreadyverified' });
+    const { email, attempt, forgot } = req.body;
 
-    const { email } = req.body;
+    if (!forgot){
+        if (!req.session.email) return res.status(401).send({ message: 'notloggedin' });
+        if (req.session.verified) return res.status(401).send({ message: 'alreadyverified' });
+    }
+
+    const user = await User.findOne({ email: email.toLowerCase() });
+    const c = user.code;
+    const exp = user.codeexp;
+
+    if(c !== attempt) return res.status(401).send({ message: 'badcode' });
+    if(new Date(exp).getTime() < Date.now()) return res.status(401).send({ message: 'expired' });
 
     await User.findOneAndUpdate(
         { email: email },
         { verified: true }, 
         { new: true, runValidators: true } 
     );
+
     req.session.verified = true;
+    req.session.email = email;
+
+    if (!forgot) req.session.loggedin = true;
+    else req.session.resetallowed = true;
+
     res.send({ message: 'goodverify' });
 
+});
+
+app.post('/api/signout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.status(500).send('Error signing out');
+        }
+    });
+    return res.send({ message: 'signedout' });
+});
+
+app.post('/api/delete', async (req, res) => {
+
+    const { password } = req.body;
+
+    if (!req.session.email) return res.status(401).send({ message: 'noemail' });
+    
+    const user = await User.findOne({ email: req.session.email });
+
+    const match = await bcrypt.compare(password, user.passhash);
+    if (!match) return res.status(401).send({ message: 'badpassw' });
+
+    await User.findOneAndDelete({ email: req.session.email });
+    
+    req.session.destroy(err => {
+        if (err) {
+            return res.status(500).send('Error signing out');
+        }
+    });
+
+    res.send({ message: 'gooddelete' });
 });
 
 app.listen(port, () => {
